6b287598a72059b0248a45e6c3cf01156e7accd6
Pádraic Brady <padraic.brady@yahoo.com>
2007-02-24 10:13:12 +0000

    * Setting up PHPUnit test environment
    git-svn-id: http://svn.astrumfutura.org/zendframework@9 0c26576f-122a-0410-90da-b7438ce1f43f

---
+++ trunk/tests/AllTests.php
@@ -0,0 +1,55 @@
+<?php
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'AllTests::main');
+}
+
+/**
+ * Read in user-defined test configuration if available; otherwise, read default test configuration
+ */
+if (is_readable('TestConfiguration.php')) {
+    require_once 'TestConfiguration.php';
+} else {
+    require_once 'TestConfiguration.php.dist';
+}
+
+require_once 'PHPUnit/Framework/TestSuite.php';
+require_once 'PHPUnit/TextUI/TestRunner.php';
+
+/**
+ * Prepend library/ to the include_path.  This allows the tests to run out of the box and
+ * helps prevent finding other copies of the framework that might be present.
+ */
+set_include_path(dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'library'
+                 . PATH_SEPARATOR . '.' . PATH_SEPARATOR . get_include_path());
+
+require_once 'ZendTest.php';
+require_once 'Zend/AllTests.php';
+
+class AllTests
+{
+    public static function main()
+    {
+        $parameters = array();
+
+        if (TESTS_GENERATE_REPORT && extension_loaded('xdebug')) {
+            $parameters['reportDirectory'] = TESTS_GENERATE_REPORT_TARGET;
+        }
+
+        PHPUnit_TextUI_TestRunner::run(self::suite(), $parameters);
+    }
+
+    public static function suite()
+    {
+        $suite = new PHPUnit_Framework_TestSuite('Zend Framework');
+
+        $suite->addTestSuite('ZendTest');
+
+        $suite->addTest(Zend_AllTests::suite());
+
+        return $suite;
+    }
+}
+
+if (PHPUnit_MAIN_METHOD == 'AllTests::main') {
+    AllTests::main();
+}
