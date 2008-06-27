34b7487d4da9212f34ef59858de0834342b72f2d
Pádraic Brady <padraic.brady@yahoo.com>
2008-06-27 15:48:19 +0000

    Updated examples
    git-svn-id: http://svn.astrumfutura.org/zendframework@206 0c26576f-122a-0410-90da-b7438ce1f43f

--- trunk/docs/examples/google.php
+++ trunk/docs/examples/google.php
@@ -5,39 +5,44 @@ require_once 'Zend/Oauth/Consumer.php';
 require_once 'Zend/Crypt/Rsa/Key/Private.php';
 
 $options = array(
-    'requestScheme' => Zend_Oauth::REQUEST_SCHEME_HEADER,
+    'requestScheme' => Zend_Oauth::REQUEST_SCHEME_HEADER, // Google does not support POSTBODY option
     'version' => '1.0',
     'signatureMethod' => 'RSA-SHA1',
-    'localUrl' => 'path/to/this/file.php',
-    'requestTokenUrl' => 'https://www.google.com/accounts/OAuthGetRequestToken?scope=http://www.google.com/m8/feeds',
+    'localUrl' => 'http://path/to/this/file.php', // UPDATE FOR YOUR DETAILS
+    'requestTokenUrl' => 'https://www.google.com/accounts/OAuthGetRequestToken',
     'userAuthorisationUrl' => 'https://www.google.com/accounts/OAuthAuthorizeToken',
     'accessTokenUrl' => 'https://www.google.com/accounts/OAuthGetAccessToken',
-    'consumerKey' => 'dev.phpspec.org',
+    'consumerKey' => 'exmaple.com',  // UPDATE FOR YOUR DETAILS
     'consumerSecret' => new Zend_Crypt_Rsa_Key_Private(
-        file_get_contents(realpath('./myrsakey.pem'));
+        file_get_contents(realpath('./myrsakey.pem'))  // UPDATE FOR YOUR DETAILS
     )
 );
 
+/**
+ * Example utilises the Google Contacts API
+ */
+
 $consumer = new Zend_Oauth_Consumer($options);
 
-if (!isset($_SESSION['ACCESS_TOKEN'])) {
+if (!isset($_SESSION['ACCESS_TOKEN_GOOGLE'])) {
     if (!empty($_GET)) {
-        $token = $consumer->getAccessToken($_GET, unserialize($_SESSION['REQUEST_TOKEN']));
-        $_SESSION['ACCESS_TOKEN'] = serialize($token);
+        $token = $consumer->getAccessToken($_GET, unserialize($_SESSION['REQUEST_TOKEN_GOOGLE']));
+        $_SESSION['ACCESS_TOKEN_GOOGLE'] = serialize($token);
     } else {
-        $token = $consumer->getRequestToken();
-        $_SESSION['REQUEST_TOKEN'] = serialize($token);
+        $token = $consumer->getRequestToken(array('scope'=>'http://www.google.com/m8/feeds'));
+        $_SESSION['REQUEST_TOKEN_GOOGLE'] = serialize($token);
         $consumer->redirect();
+        exit;
     }
 } else {
-    $token = unserialize($_SESSION['ACCESS_TOKEN']);
-    $_SESSION['ACCESS_TOKEN'] = null;
+    $token = unserialize($_SESSION['ACCESS_TOKEN_GOOGLE']);
+    $_SESSION['ACCESS_TOKEN_GOOGLE'] = null;
 }
 
+// OAuth Access Token Retreived; Proceed to query Data API
 $client = $token->getHttpClient($options);
-$client->setUri('http://');
-$client->setMethod(Zend_Http_Client::POST);
-//$client->setParameterPost();
+$client->setUri('http://www.google.com/m8/feeds/groups/default/full');
+$client->setMethod(Zend_Http_Client::GET);
 
 $response = $client->request();
 header('Content-Type: ' . $response->getHeader('Content-Type'));
--- trunk/docs/examples/magnolia.php
+++ trunk/docs/examples/magnolia.php
@@ -7,42 +7,36 @@ $options = array(
     'requestScheme' => Zend_Oauth::REQUEST_SCHEME_HEADER,
     'version' => '1.0',
     'signatureMethod' => 'HMAC-SHA1',
-    'localUrl' => 'http://your/path/to/this/file.php',
+    'localUrl' => 'http://your/path/to/this/file.php', // UPDATE FOR YOUR DETAILS
     'requestTokenUrl' => 'http://ma.gnolia.com/oauth/get_request_token',
     'userAuthorisationUrl' => 'http://ma.gnolia.com/oauth/authorize',
     'accessTokenUrl' => 'http://ma.gnolia.com/oauth/get_access_token',
-    'consumerKey' => 'YOUR_CONSUMER_KEY',
-    'consumerSecret' => 'YOUR_CONSUMER_KEY_SECRET'
+    'consumerKey' => 'YOUR_CONSUMER_KEY', // UPDATE FOR YOUR DETAILS
+    'consumerSecret' => 'YOUR_CONSUMER_KEY_SECRET' // UPDATE FOR YOUR DETAILS
 );
 
 $consumer = new Zend_Oauth_Consumer($options);
 
-if (!isset($_SESSION['ACCESS_TOKEN'])) {
+if (!isset($_SESSION['ACCESS_TOKEN_MAGNOLIA'])) {
     if (!empty($_GET)) {
-        $token = $consumer->getAccessToken($_GET, unserialize($_SESSION['REQUEST_TOKEN']));
-        $_SESSION['ACCESS_TOKEN'] = serialize($token);
+        $token = $consumer->getAccessToken($_GET, unserialize($_SESSION['REQUEST_TOKEN_MAGNOLIA']));
+        $_SESSION['ACCESS_TOKEN_MAGNOLIA'] = serialize($token);
     } else {
         $token = $consumer->getRequestToken();
-        $_SESSION['REQUEST_TOKEN'] = serialize($token);
+        $_SESSION['REQUEST_TOKEN_MAGNOLIA'] = serialize($token);
         $consumer->redirect();
     }
 } else {
-    $token = unserialize($_SESSION['ACCESS_TOKEN']);
-    $_SESSION['ACCESS_TOKEN'] = null; // forces reset of access token on all example runs ;)
+    $token = unserialize($_SESSION['ACCESS_TOKEN_MAGNOLIA']);
+    $_SESSION['ACCESS_TOKEN_MAGNOLIA'] = null;
 }
 
 $client = $token->getHttpClient($options);
 $client->setUri('http://ma.gnolia.com/api/rest/2/bookmarks_count');
+$client->setRequestScheme(Zend_Oauth::REQUEST_SCHEME_POSTBODY);
 $client->setMethod(Zend_Http_Client::POST);
 $client->setParameterPost('group','oauth');
 
 $response = $client->request();
 header('Content-Type: ' . $response->getHeader('Content-Type'));
 echo $response->getBody();
-
-// Retrieved XML will be similar to:
-
-//      <?xml version="1.0" encoding="utf-8" ?>
-//      <response status="ok" version="">
-//      	<count>138</count>
-//      </response>
