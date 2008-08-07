eae81faeb54482695b9fbae4c65144618f58c6a0
J. Stutterheim <j.stutterheim@hccnet.nl>
2008-08-07 22:30:20 +0200

    Updated Atom support

--- trunk/library/Proposed/Zend/Feed/Reader/Feed/Atom.php
+++ trunk/library/Proposed/Zend/Feed/Reader/Feed/Atom.php
@@ -11,6 +11,20 @@ require_once 'Zend/Feed/Reader/Feed.php';
  */
 class Zend_Feed_Reader_Feed_Atom extends Zend_Feed_Reader_Feed
 {
+    public function getAuthors()
+    {
+        /**
+         * TODO: Add authors support.
+         * Atom seems to store author info both in the "common" section and in
+         * the entries.
+         */
+    }
+    
+    public function getAuthor()
+    {
+        
+    }
+    
     public function getCopyright()
     {
         if (isset($this->_data['copyright'])) {
@@ -31,6 +45,44 @@ class Zend_Feed_Reader_Feed_Atom extends Zend_Feed_Reader_Feed
         
         $this->_data['copyright'] = $copyright;
         return $this->_data['copyright'];
+    }
+    
+    public function getDescription()
+    {
+        if (isset($this->_data['description'])) {
+            return $this->_data['description'];
+        }
+        
+        $description = null;
+        
+        if ($this->getType() === Zend_Feed_Reader::TYPE_ATOM_03) {
+            $description = $this->_xpath->evaluate('string(/feed/tagline)'); // TODO: Is this the same as subtitle?
+        } else {
+            $description = $this->_xpath->evaluate('string(/feed/subtitle)');
+        }
+        
+        if (!$description) {
+            $description = null;
+        }
+        
+        $this->_data['description'] = $description;
+        return $this->_data['description'];
+    }
+    
+    public function getLanguage()
+    {
+        if (isset($this->_data['language'])) {
+            return $this->_data['language'];
+        }
+        
+        $language = $this->_xpath->evaluate('string(/feed/lang)');
+        
+        if (!$language) {
+            $language = null;
+        }
+        
+        $this->_data['language'] = $language;
+        return $this->_data['language'];
     }
     
     public function getLink()
@@ -89,11 +141,25 @@ class Zend_Feed_Reader_Feed_Atom extends Zend_Feed_Reader_Feed
 
     protected function _registerDefaultNamespaces()
     {
+        switch ($this->_data['type']) {
+            case Zend_Feed_Reader::TYPE_ATOM_10:
+                $this->_xpath->registerNamespace('atom', Zend_Feed_Reader::NAMESPACE_ATOM_10);
+                break;
+            case Zend_Feed_Reader::TYPE_ATOM_03:
+                $this->_xpath->registerNamespace('atom', Zend_Feed_Reader::NAMESPACE_ATOM_03);
+                break;
+        }
+        $this->_xpath->registerNamespace('dc10', Zend_Feed_Reader::NAMESPACE_DC_10);
+        $this->_xpath->registerNamespace('dc11', Zend_Feed_Reader::NAMESPACE_DC_11);
+    }
+    
+    protected function _indexEntries()
+    {
+        $entries = array();
         $entries = $this->_xpath->evaluate('//entry');
         
         foreach($entries as $index=>$entry) {
             $this->_entries[$index] = $entry;
         }
     }
-
 }
