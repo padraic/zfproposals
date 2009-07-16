50d30d9d08b71ae0402e3baa02b8e6efcb51576e
Padraic Brady <padraic@padraic-desktop.(none)>
2009-07-16 19:32:21 +0100

    skeleton Entry class

---
+++ library/Zend/Feed/Writer/Entry.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to padraic dot brady at yahoo dot com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Feed_Writer_Entry
+ * @copyright  Copyright (c) 2009 Padraic Brady
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+/**
+ * @see Zend_Date
+ */
+require_once 'Zend/Date.php';
+
+/**
+ * @see Zend_Date
+ */
+require_once 'Zend/Uri.php';
+
+/**
+ * @category   Zend
+ * @package    Zend_Feed_Writer_Entry
+ * @copyright  Copyright (c) 2009 Padraic Brady
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+class Zend_Feed_Writer_Entry
+{
+
+    protected $_data = array();
+
+}
