5a541b93dc7ff617213b9d1cf067c58066e2603a
Padraic Brady <padraic@padraic-desktop.(none)>
2009-08-01 18:21:37 +0100

    Adding some keypair storage class skeletons for later

---
+++ library/Zend/Pubsubhubbub/Storage/File.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to padraic dot brady at yahoo dot com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Pubsubhubbub
+ * @copyright  Copyright (c) 2009 Padraic Brady
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+/**
+ * NOTE: Mainly for testing; will add a other versions at some point using
+ * Zend_Db and Zend_Cache. This is largely a simple keypair store anyway.
+ * If stuck, use the interface to roll your own...
+ */
+
+/**
+ * @see Zend_Pubsubhubbub_StorageInterface
+ */
+require_once 'Zend/Pubsubhubbub/StorageInterface.php';
+
+/**
+ * @category   Zend
+ * @package    Zend_Pubsubhubbub
+ * @copyright  Copyright (c) 2009 Padraic Brady
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+class Zend_Pubsubhubbub_Storage_File implements Zend_Pubsubhubbub_StorageInterface
+{
+
+
+
+}
