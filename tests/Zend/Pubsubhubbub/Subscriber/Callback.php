78efd2f0f6d9a5f41229e04abd720ed621913d5d
Padraic Brady <padraic@padraic-desktop.(none)>
2009-08-01 21:48:25 +0100

    Adding skeleton for a Callback class for Subscriber implementation

---
+++ tests/Zend/Pubsubhubbub/Subscriber/Callback.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to padraic dot brady at yahoo dot com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Pubsubhubbub
+ * @copyright  Copyright (c) 2009 Padraic Brady
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+/**
+ * @see Zend_Pubsubhubbub
+ */
+require_once 'Zend/Pubsubhubbub.php';
+
+/**
+ * @category   Zend
+ * @package    Zend_Pubsubhubbub
+ * @copyright  Copyright (c) 2009 Padraic Brady
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+class Zend_Pubsubhubbub_Subscriber_Callback
+{
+
+    /**
+     * Handle any callback from a Hub Server responding to a subscription or
+     * unsubscription request. This should be the Hub Server confirming the
+     * the request prior to taking action on it.
+     *
+     */
+    public function handle()
+    {
+
+    }
+
+
+}
