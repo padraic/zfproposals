b5bb0fb39dc939687962b185c3d125eb3911336d
Padraic Brady <padraic@padraic-desktop.(none)>
2009-08-03 13:27:48 +0100

    Added tests over Filesystem Storage class

--- tests/Zend/Pubsubhubbub/Storage/FilesystemTest.php
+++ tests/Zend/Pubsubhubbub/Storage/FilesystemTest.php
@@ -5,12 +5,35 @@ require_once 'Zend/Pubsubhubbub/Storage/Filesystem.php';
 
 class Zend_Pubsubhubbub_Storage_FilesystemTest extends PHPUnit_Framework_TestCase
 {
+    protected $_store = null;
+
+    public function setUp()
+    {
+        $this->_store = new Zend_Pubsubhubbub_Storage_Filesystem;
+    }
 
     public function testImplementsStorageInterface()
     {
-        $store = new Zend_Pubsubhubbub_Storage_Filesystem;
-        $this->assertTrue($store instanceof Zend_Pubsubhubbub_StorageInterface);
+        $this->assertTrue($this->_store instanceof Zend_Pubsubhubbub_StorageInterface);
     }
 
-    // add the rest later - it's a really simple class. Too lazy to TDD its ass...copied from my OpenID impl.
+    public function testCanStoreVerifyToken()
+    {
+        $this->_store->setVerifyToken('key1', 'value1');
+        $this->assertEquals('value1', $this->_store->getVerifyToken('key1'));
+    }
+
+    public function testCanCheckExistenceOfVerifyToken()
+    {
+        $this->_store->setVerifyToken('key2', 'value2');
+        $this->assertTrue($this->_store->hasVerifyToken('key2'));
+    }
+
+    public function testCanDeleteVerifyToken()
+    {
+        $this->_store->setVerifyToken('key3', 'value3');
+        $this->_store->removeVerifyToken('key3');
+        $this->assertFalse($this->_store->hasVerifyToken('key3'));
+    }
+
 }
